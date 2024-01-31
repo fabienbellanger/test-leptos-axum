45470fa00a6ebaca2248f76fef9a3eeaf9cb9512
Fabien Bellanger <fabien.bellanger@apitic.com>
2024-01-31 17:38:55 +0100

    Initial commit

---
+++ src/app.rs
@@ -0,0 +1,65 @@
+use crate::error_template::{AppError, ErrorTemplate};
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+
+
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/test-leptos-axum.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <Router fallback=|| {
+            let mut outside_errors = Errors::default();
+            outside_errors.insert_with_default_key(AppError::NotFound);
+            view! {
+                <ErrorTemplate outside_errors/>
+            }
+            .into_view()
+        }>
+            <main>
+                <Routes>
+                    <Route path="" view=HomePage/>
+                    <Route path="test" view=TestPage/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(0);
+    let on_click = move |_| set_count.update(|count| *count += 1);
+
+    view! {
+        <h1>"Welcome to Leptos!"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+        <A href="test">Test page</A>
+    }
+}
+
+
+
+/// Renders the test page of your application.
+#[component]
+fn TestPage() -> impl IntoView {
+    let navigate = use_navigate();
+    view! {
+        <h1>"Welcome to test page!"</h1>
+
+        <button on:click=move |_| navigate("", Default::default())>Home</button>
+    }
+}
