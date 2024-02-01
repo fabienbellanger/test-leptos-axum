3e265ccf6093cd8da9267a0e56a1746129108e36
Fabien Bellanger <fabien.bellanger@apitic.com>
2024-02-01 13:44:37 +0100

    Add style

--- src/app.rs
+++ src/app.rs
@@ -3,14 +3,16 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
+const BUTTON_CLASS: &str =
+    "px-6 py-2 text-blue-100 bg-blue-500 hover:bg-blue-700 rounded shadow shadow-blue-500/50";
+const DIV_COUNTER_CLASS: &str = "mt-4 text-2xl font-bold";
+
 #[component]
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
 
     view! {
-
-
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/test-leptos-axum.css"/>
@@ -45,21 +47,21 @@ fn HomePage() -> impl IntoView {
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
-        <A href="test">Test page</A>
+        <h1 class=DIV_COUNTER_CLASS>"Welcome to Leptos!"</h1>
+        <button on:click=on_click class=BUTTON_CLASS>"Click Me: " {count}</button>
+        <div>
+            <A href="test">Test page</A>
+        </div>
     }
 }
-
-
 
 /// Renders the test page of your application.
 #[component]
 fn TestPage() -> impl IntoView {
     let navigate = use_navigate();
     view! {
-        <h1>"Welcome to test page!"</h1>
+        <h1 class=DIV_COUNTER_CLASS>"Welcome to test page!"</h1>
 
-        <button on:click=move |_| navigate("", Default::default())>Home</button>
+        <button on:click=move |_| navigate("", Default::default()) class=BUTTON_CLASS>Home</button>
     }
 }
